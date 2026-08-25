b616658e38903e07e42be8a173326a47485a7ed4
Allen Chen <allenbc528@gmail.com>
2026-08-25 10:35:08 -0700

    update hello world app

--- Module01-Setup/hello_world.cpp
+++ Module01-Setup/hello_world.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float num1;
+    float num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << (num1 + num2) << endl;
+    return 0;
+}
